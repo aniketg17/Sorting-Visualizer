b37ea071aa6f5fe57c2ef0fb5ab1ade59f90e23d
aniketg17 <aniketkg@gmail.com>
2020-05-30 20:32:59 +0530

    Implemented insertion sort to test GUI

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -1,6 +1,15 @@
 public class InsertionSort extends Sorter {
     @Override
     public void sort(int[] array) {
-
+        for (int i = 0; i < array.length; i++) {
+            int j = i - 1;
+            int key = array[i];
+            while (j >= 0 && array[j] > key) {
+                array[j] = array[j] ^ array[j+1];
+                array[j+1] = array[j] ^ array[j+1];
+                array[j] = array[j] ^ array[j+1];
+                --j;
+            }
+        }
     }
 }
--- src/Sorter.java
+++ src/Sorter.java
@@ -1,5 +1,4 @@
 public abstract class Sorter {
     private int arraySize;
     public abstract void sort(int[] array);
-
 }
