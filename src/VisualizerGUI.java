55b51fd84ab4b896be47cb7fc81c6c7044cde644
aniketg17 <aniketkg@gmail.com>
2020-05-30 23:21:31 +0530

    Rough main screen design including checkboxes and sort button ready

--- src/VisualizerGUI.java
+++ src/VisualizerGUI.java
@@ -4,7 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class VisualizerGUI {
-    JCheckBox[] sortingAlgorithmChoices = new JCheckBox[6];
+    static JCheckBox[] sortingAlgorithmChoices = new JCheckBox[6];
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
@@ -31,10 +31,31 @@ public class VisualizerGUI {
         mainTitle.setFont(new Font("Courier", Font.BOLD, 30));
         mainPanel.add(mainTitle);
 
-        JPanel checkboxes = new JPanel(new GridLayout(2,3));
+        sortingAlgorithmChoices[0] = new JCheckBox(new BubbleSort().toString());
+        sortingAlgorithmChoices[1] = new JCheckBox(new HeapSort().toString());
+        sortingAlgorithmChoices[2] = new JCheckBox(new InsertionSort().toString());
+        sortingAlgorithmChoices[3] = new JCheckBox(new MergeSort().toString());
+        sortingAlgorithmChoices[4] = new JCheckBox(new RadixSort().toString());
+        sortingAlgorithmChoices[5] = new JCheckBox(new RandomizedQuicksort().toString());
 
 
+        JPanel checkboxPanel = new JPanel(new GridLayout(0,2, 5 , 5));
+        checkboxPanel.setAlignmentX(JComponent.CENTER_ALIGNMENT);
+
+        for (JCheckBox checkBox : sortingAlgorithmChoices) {
+            checkBox.setAlignmentX(JComponent.CENTER_ALIGNMENT);
+            checkboxPanel.add(checkBox);
+        }
+        mainPanel.add(checkboxPanel);
+        mainPanel.setAlignmentX(JComponent.CENTER_ALIGNMENT);
+
+
+        JPanel buttonHolder = new JPanel();
+        JButton submit = new JButton("Sort!");
+        buttonHolder.add(submit);
+        mainPanel.add(buttonHolder);
 
         mainScreen.setContentPane(mainPanel);
+
     }
 }
